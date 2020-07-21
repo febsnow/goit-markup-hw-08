dcdfdbedbb037b31cac83843b9eb8e5db60c5aeb
Smekalin Sergei <crypto.mnr@gmail.com>
2020-07-21 22:22:43 +0300

    Initial commit
    First copy from HW-07

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+(() => {
+  document.querySelector('.clients-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    console.log('');
+    console.count('Submit #');
+
+    new FormData(e.currentTarget).forEach((value, name) => console.log(name, ':', value));
+  });
+})();
